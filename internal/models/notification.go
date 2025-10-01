8197ca0c4d6647031fc2e92579507646f0eb0ec8
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2025-10-01 17:55:46 +0300

    fix: model

--- internal/models/notification.go
+++ internal/models/notification.go
@@ -1,11 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type Notification struct {
-    id        uint      `gorm:"primaryKey" json:"id"`
-    sender    string    `gorm:"size:255;not null" json:"sender"`
-    recipient string    `gorm:"size:255;not null" json:"recipient"`
-    message   string    `gorm:"type:text;not null" json:"message"`
-    CreatedAt time.Time `json:"created_at"`
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Sender    string    `gorm:"size:255;not null" json:"sender" validate:"required,email"`
+	Recipient string    `gorm:"size:255;not null" json:"recipient" validate:"required,email"`
+	Message   string    `gorm:"type:text;not null" json:"message"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func (n *Notification) Validate() error {
+	v := validator.New()
+	return v.Struct(n)
 }
